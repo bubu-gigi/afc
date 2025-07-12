11eb83392bb98c89b65aec756e829666d009f7ea
Guglielmo Borgognoni <guglielmo.borgognoni@grupposcai.it>
2025-07-12 12:41:23 +0200

    update utils and types

--- converters/types.go
+++ converters/types.go
@@ -1,5 +1,10 @@
 package converters
 
+import (
+	"encoding/xml"
+	"time"
+)
+
 type ShellLinkHeader struct {
 	HeaderSize     uint32
 	LinkCLSID      [16]byte
@@ -82,4 +87,128 @@ type ShellLink struct {
 	LinkTargetIdList LinkTargetIdList
 	LinkInfo LinkInfo
 	StringData StringData
+}
+
+//Jobs v.1
+
+type JobHeader struct {
+	ProductVersion      uint16
+	FileVersion 		uint16
+	UUID                [16]byte
+	AppNameOffset       uint32
+	TriggerOffset       uint32
+	ErrorRetryCount     uint16
+	ErrorRetryInterval  uint16
+	IdleDeadline        uint16
+	IdleWait            uint16
+	Priority            uint32
+	MaxRunTime          uint32
+	ExitCode 			uint32 
+	Status 				uint32
+	Flags  				uint32
+}
+
+type JobTrigger struct {
+	TriggerSize       uint16 // always 0x30
+	Reserved1         uint16
+	BeginYear         uint16
+	BeginMonth        uint16
+	BeginDay          uint16
+	EndYear           uint16
+	EndMonth          uint16
+	EndDay            uint16
+	StartHour         uint16
+	StartMinute       uint16
+	MinutesDuration   uint32
+	MinutesInterval   uint32
+	Flags             uint32
+	TriggerType       uint32
+	TriggerSpecific0  uint16
+	TriggerSpecific1  uint16
+	TriggerSpecific2  uint16
+	Padding           uint16
+	Reserved2         uint16
+	Reserved3         uint16
+}
+
+// Jobs v.2 XML
+
+type Task struct {
+	XMLName      xml.Name     `xml:"Task"`
+	Registration Registration `xml:"RegistrationInfo"`
+	Principals   Principals   `xml:"Principals"`
+	Actions      Actions      `xml:"Actions"`
+	Triggers     Triggers     `xml:"Triggers"`
+}
+
+type Registration struct {
+	Author string `xml:"Author"`
+}
+
+type Principals struct {
+	Principal []Principal `xml:"Principal"`
+}
+
+type Principal struct {
+	UserID    string `xml:"UserId"`
+	LogonType string `xml:"LogonType"`
+}
+
+type Triggers struct {
+	TimeTrigger []TimeTrigger `xml:"TimeTrigger"`
+}
+
+type TimeTrigger struct {
+	StartBoundary string `xml:"StartBoundary"`
+	Enabled       string `xml:"Enabled"`
+}
+
+type Actions struct {
+	Exec []ExecAction `xml:"Exec"`
+}
+
+type ExecAction struct {
+	Command   string `xml:"Command"`
+	Arguments string `xml:"Arguments"`
+}
+
+// UsrJrnl
+
+type UsnRecord struct {
+	RecordLength           uint32
+	MajorVersion           uint16
+	MinorVersion           uint16
+	FileReferenceNumber    uint64
+	ParentFileReferenceNum uint64
+	Usn                    uint64
+	Timestamp              time.Time
+	Reason                 uint32
+	SourceInfo             uint32
+	SecurityID             uint32
+	FileAttributes         uint32
+	FileName               string
+}
+
+var ReasonFlags = map[uint32]string{
+	0x00000001: "DataOverwrite",
+	0x00000002: "DataExtend",
+	0x00000004: "DataTruncation",
+	0x00000010: "NamedDataOverwrite",
+	0x00000020: "NamedDataExtend",
+	0x00000040: "NamedDataTruncation",
+	0x00000100: "FileCreate",
+	0x00000200: "FileDelete",
+	0x00000400: "EaChange",
+	0x00000800: "SecurityChange",
+	0x00001000: "RenameOldName",
+	0x00002000: "RenameNewName",
+	0x00004000: "IndexableChange",
+	0x00008000: "BasicInfoChange",
+	0x00010000: "HardLinkChange",
+	0x00020000: "CompressionChange",
+	0x00040000: "EncryptionChange",
+	0x00080000: "ObjectIdChange",
+	0x00100000: "ReparsePointChange",
+	0x00200000: "StreamChange",
+	0x80000000: "Close",
 }
--- lib/utils.go
+++ lib/utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/binary"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"path/filepath"
@@ -10,26 +11,35 @@ import (
 	"time"
 	"unicode"
 	"unicode/utf16"
+
+	"www.velocidex.com/golang/go-ntfs/parser"
 )
 
-func CreateOutputFile(file string)*os.File {
-  base := filepath.Base(file)
-  fileCreated, err := os.Create("output/" + removeFileExtention(base) + ".csv")
+func CreateOutputFile(file string) *os.File {
+	relPath, err := filepath.Rel("./data", file)
+	if err != nil {
+		fmt.Printf("Error resolving relative path: %v\n", err)
+		os.Exit(1)
+	}
+
+	outputPath := filepath.Join("output", relPath)
+
+	outputPath = strings.TrimSuffix(outputPath, filepath.Ext(outputPath)) + ".csv"
+
+	outputDir := filepath.Dir(outputPath)
+	if err := os.MkdirAll(outputDir, 0755); err != nil {
+		fmt.Printf("Error creating output directories: %v\n", err)
+		os.Exit(1)
+	}
+
+	fileCreated, err := os.Create(outputPath)
 	if err != nil {
 		fmt.Printf("Error creating the csv file: %v\n", err)
 		os.Exit(1)
 	}
-  return fileCreated
-}
 
-func removeFileExtention(file string) string {
-  parts := strings.Split(file, ".")
-  if len(parts) <= 1 {
-    return file 
-  }
-  return strings.Join(parts[:len(parts)-1], ".")
+	return fileCreated
 }
-
 
 func CheckNullUint16(v uint16) {
 	if v != 0 {
@@ -95,4 +105,55 @@ func IsPrintable(s string) bool {
 		}
 	}
 	return true
+}
+
+
+
+//VELOCIDEX
+
+func GetMFTEntry(ntfs_ctx *parser.NTFSContext, filename string) (*parser.MFT_ENTRY, error) {
+	mft_idx, _, _, _, err := parser.ParseMFTId(filename)
+	if err == nil {
+		// Access by mft id (e.g. 1234-128-6)
+		return ntfs_ctx.GetMFT(mft_idx)
+	} else {
+		// Access by filename.
+		dir, err := ntfs_ctx.GetMFT(5)
+		if err != nil {
+			return nil, err
+		}
+
+		return dir.Open(ntfs_ctx, filename)
+	}
+}
+
+
+func ReadFullUnicodeString(f *os.File) (string, error) {
+	var charCount uint16
+	err := binary.Read(f, binary.LittleEndian, &charCount)
+	if err != nil {
+		return "", err
+	}
+
+	if charCount == 0 {
+		return "", nil
+	}
+
+	byteCount := int(charCount) * 2
+	buf := make([]byte, byteCount)
+	_, err = io.ReadFull(f, buf)
+	if err != nil {
+		return "", err
+	}
+
+	utf16buf := make([]uint16, charCount)
+	for i := 0; i < int(charCount); i++ {
+		utf16buf[i] = binary.LittleEndian.Uint16(buf[i*2 : i*2+2])
+	}
+
+	if charCount > 0 && utf16buf[charCount-1] == 0 {
+		utf16buf = utf16buf[:charCount-1]
+	}
+
+	return string(utf16.Decode(utf16buf)), nil
 }
